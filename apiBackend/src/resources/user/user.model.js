629d1d21e693d68fed8c13de67c38161b6f1f27b
CENWACHUKWU <CENWACHUKWUU@GMAIL.COM>
2020-02-28 14:47:13 -0500

    setting user model

--- apiBackend/src/resources/user/user.model.js
+++ apiBackend/src/resources/user/user.model.js
@@ -3,7 +3,47 @@
 import mongoose from "mongoose";
 import bcrypt from "bcrypt";
 
-const userSchema = new mongoose.Schema({});
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true
+    },
+
+    password: {
+      type: String,
+      required: true
+    },
+
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true
+    },
+
+    settings: {
+      theme: {
+        type: String,
+        required: true,
+        default: "dark"
+      },
+      notifications: {
+        type: Boolean,
+        required: true,
+        default: true
+      },
+      compactMode: {
+        type: Boolean,
+        required: true,
+        default: false
+      }
+    }
+  },
+  { timestamps: true }
+);
 
 // hash password with bcrypt
 userSchema.pre("save", function(next) {});
