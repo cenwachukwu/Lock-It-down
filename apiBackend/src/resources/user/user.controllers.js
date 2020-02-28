7525e9ca00db2ab10fd31e62998beed14e22ff67
CENWACHUKWU <CENWACHUKWUU@GMAIL.COM>
2020-02-28 15:11:37 -0500

    checking out code for getting all the users

--- apiBackend/src/resources/user/user.controllers.js
+++ apiBackend/src/resources/user/user.controllers.js
@@ -8,6 +8,19 @@
 
 import { User } from "./user.model";
 
+// trying out code for getting all the users
+export const users = async (req, res) => {
+  try {
+    const allUsers = await User.find()
+      .lean()
+      .exec();
+    res.status(200).json({ data: allUsers });
+  } catch (e) {
+    console.error(e);
+    res.status(400).end();
+  }
+};
+
 export const person = async (req, res) => {
   res.status(200).json({ data: req.user });
 };
