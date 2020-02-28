41b74abc83b51960561f3b5d6e25dc1466a8f996
CENWACHUKWU <CENWACHUKWUU@GMAIL.COM>
2020-02-28 15:35:26 -0500

    import user for auth

--- apiBackend/dist/resources/notes/notes.controllers.js
+++
@@ -1,7 +0,0 @@
-// controllers are just middleware but with the intent of returning data
-// so they handle what a route + verb combo can access from the db
-// controllers implements the logic that interacts with our database models
-// instead of writing a controller for everysingle crud function, we can have generalized controlers
-// import model
-// import generic controllers from utils/crud
-"use strict";
--- apiBackend/dist/utils/auth.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-var _config = _interopRequireDefault(require("../config"));
-
-var _jsonwebtoken = _interopRequireDefault(require("jsonwebtoken"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
