a23b65123010003acdd274e000f67cade8f4249b
CENWACHUKWU <CENWACHUKWUU@GMAIL.COM>
2020-02-28 12:51:44 -0500

    app used cors/json/urlencoded/morgan/express

--- apiBackend/src/server.js
+++ apiBackend/src/server.js
@@ -3,6 +3,13 @@ import { json, urlencoded } from "body-parser";
 import morgan from "morgan";
 import cors from "cors";
 
+export const app = express();
+
+app.use(cors());
+app.use(json());
+app.use(urlencoded({ extended: true }));
+app.use(morgan("dev"));
+
 export const start = async () => {
   try {
     await connect();
