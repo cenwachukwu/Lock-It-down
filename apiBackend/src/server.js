2b6d681f51f16fd19ac9e500cf9d84718885cae7
CENWACHUKWU <CENWACHUKWUU@GMAIL.COM>
2020-02-28 12:49:56 -0500

    set up server

--- apiBackend/src/server.js
+++ apiBackend/src/server.js
@@ -2,3 +2,14 @@ import express from "express";
 import { json, urlencoded } from "body-parser";
 import morgan from "morgan";
 import cors from "cors";
+
+export const start = async () => {
+  try {
+    await connect();
+    app.listen(config.port, () => {
+      console.log(`REST API on http://localhost:${config.port}/api`);
+    });
+  } catch (e) {
+    console.error(e);
+  }
+};
