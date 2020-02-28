1cda1f452defb6f7708ca212cfa58634163d1de1
CENWACHUKWU <CENWACHUKWUU@GMAIL.COM>
2020-02-28 13:58:57 -0500

    logic for model and imported mongoose for notes

---
+++ apiBackend/src/resources/notes/notes.model.js
@@ -0,0 +1,4 @@
+// data modelling: it's a way to think about the structure of our data and the relationships between different objects in our application and how we'll store that data and embody those relationships in our database.
+// we will need to set up a relationship with the user
+
+import mongoose from "mongoose";
