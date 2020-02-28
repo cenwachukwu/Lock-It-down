2e826060b68077dd0b82e2309f10969eabd19f4e
CENWACHUKWU <CENWACHUKWUU@GMAIL.COM>
2020-02-28 15:39:57 -0500

    added new token auth

--- apiBackend/dist/utils/auth.js
+++ apiBackend/dist/utils/auth.js
@@ -1,22 +1,36 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.newToken = void 0;
+
+var _config = _interopRequireDefault(require("../config"));
+
+var _user = require("../resources/user/user.model");
+
+var _jsonwebtoken = _interopRequireDefault(require("jsonwebtoken"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
 // authetication is controlling if an incoming request can proceed or not
 // authorization is controlling if an authenticated request has the correct permissions to access a resource
 // jwt authentications are basically tokens passed to check auth on the server. so it's a bearer token strategy that allows the api to be statless with user auth
-import config from "../config";
-import { User } from "../resources/user/user.model";
-import jwt from "jsonwebtoken";
-
 // getting new tokens
 // newToken function takes in User from the user.model and creates a new json web token based on the user.id
 // signed with the correct secerts and xpiration time
 // user goes in, token come out
 // creates our payload
-export const newToken = user => {
-  return jwt.sign({ id: user.id }, config.secrets.jwt, {
-    expiresIn: config.secrets.jwtExp
+const newToken = user => {
+  return _jsonwebtoken.default.sign({
+    id: user.id
+  }, _config.default.secrets.jwt, {
+    expiresIn: _config.default.secrets.jwtExp
   });
-};
-
-// verifying tokens
+}; // verifying tokens
 // signup
 // signin
 // protect middleware
+
+
+exports.newToken = newToken;
