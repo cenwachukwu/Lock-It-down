aefaf2caa0bbfdd92e23fd53e63cff8b633106b2
CENWACHUKWU <CENWACHUKWUU@GMAIL.COM>
2020-02-28 13:46:04 -0500

    setup data connection

--- apiBackend/src/server.js
+++ apiBackend/src/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import { json, urlencoded } from "body-parser";
 import morgan from "morgan";
 import cors from "cors";
+import config from "./config";
+import { connect } from "./utils/db";
 
 export const app = express();
 
