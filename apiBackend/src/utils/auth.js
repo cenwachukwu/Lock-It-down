1494e35cf40b96e879712083c028834384df1086
CENWACHUKWU <CENWACHUKWUU@GMAIL.COM>
2020-02-28 13:34:11 -0500

    imported dependencies for auth

--- apiBackend/src/utils/auth.js
+++ apiBackend/src/utils/auth.js
@@ -2,5 +2,4 @@
 // authorization is controlling if an authenticated request has the correct permissions to access a resource
 // jwt authentications are basically tokens passed to check auth on the server. so it's a bearer token strategy that allows the api to be statless with user auth
 import config from "../config";
-import { User } from "../resources/user/user.model";
 import jwt from "jsonwebtoken";
