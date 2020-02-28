e89a27684b6dfbf3932694bf8ceb7e5c2fe84c28
CENWACHUKWU <CENWACHUKWUU@GMAIL.COM>
2020-02-28 14:17:09 -0500

    pseudocode, logic, and skeleton for notes routing

---
+++ apiBackend/src/resources/notes/notes.router.js
@@ -0,0 +1,8 @@
+// Express is a minimalistic web framework and this basically means you will be building out the functionality that you want
+// we are basically routing our requests with express.js
+
+import { Router } from "express";
+
+const router = Router();
+
+export default router;
