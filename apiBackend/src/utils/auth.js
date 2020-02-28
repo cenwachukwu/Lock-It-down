44ef62b5c4fd6e909e94048687f40b1a1cf85a58
CENWACHUKWU <CENWACHUKWUU@GMAIL.COM>
2020-02-28 13:35:50 -0500

    pseudocode for auth

--- apiBackend/src/utils/auth.js
+++ apiBackend/src/utils/auth.js
@@ -3,3 +3,9 @@
 // jwt authentications are basically tokens passed to check auth on the server. so it's a bearer token strategy that allows the api to be statless with user auth
 import config from "../config";
 import jwt from "jsonwebtoken";
+
+// getting new tokens
+// verifying tokens
+// signup
+// signin
+// protect middleware
